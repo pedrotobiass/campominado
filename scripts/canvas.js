1f9b10a35fda7dde371d37886f7e8d1807b841a6
pedrotobiass <pedrotobiassilvadejesus@gmail.com>
2025-04-29 16:59:53 -0300

    graphics library idemtifier

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -3,6 +3,11 @@ const canvas = document.querySelector('#campominado')
 /** @type {undefined|WebGL2RenderingContext} */
 const gl = canvas.getContext('webgl2')
 
+const span = document.querySelector('#gl')
+span.innerText = 'Using webgl-2'
+
 if(!gl) {
     alert('Your browser does not support webgl')
+} else {
+
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+import "./canvas"
+
 function main() {
 }
 
