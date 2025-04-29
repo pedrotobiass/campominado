2f0d7d3e33d2c2982f1713668bd9e4f697dcf72a
pedrotobiass <pedrotobiassilvadejesus@gmail.com>
2025-04-29 17:04:28 -0300

    use p instead of span

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -3,10 +3,10 @@ const canvas = document.querySelector('#campominado')
 /** @type {undefined|WebGL2RenderingContext} */
 const gl = canvas.getContext('webgl2')
 
-const span = document.querySelector('#gl')
+const p = document.querySelector('#gl')
 
 if(!gl) {
     alert('Your browser does not support webgl')
 } else {
-    span.innerText = 'Using webgl2'
+    p.innerText = 'Using webgl2'
 }
